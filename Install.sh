34b198c403101d4f7ebe88988a4d5e775d6efedd
642040 <642040@cox.net>
2016-04-17 06:22:20 -0700

    Working on automated install

--- Install.sh
+++ Install.sh
@@ -3,6 +3,26 @@
 # ex:  git clone http://github.com/642040/PiTimer
 
 git pull origin
-rsync -a --delete PiTimerWWW/ /var/www/PiTimer/
-rsync -a --delete PiTimerCron/ /root/PiTimerCron/
+
+# Install/update prerequisites
+
+apt-get update
+apt-get -y install apache2 build-essential
+
+cd ~
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.50.tar.gz
+tar zxvf bcm2835-1.50.tar.gz
+cd bcm2835-1.50
+./configure
+make
+make check
+make install
+cd -
+
+cpan Device::BCM2835
+
+ln -s PiTimerWWW/ /var/www/PiTimer/
+ln -s PiTimerCron/ /root/PiTimerCron/
+
 # setup crontabs
+crontab crontab.config
